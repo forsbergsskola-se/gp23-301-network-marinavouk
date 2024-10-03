c79bc14bb9837111250c1fc96151ab74c227e6e0
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-10-03 19:01:42 +0200

    created a lobby, join a lobby, remove a lobby

--- Multiplayer/GameLobbyServer/Program.cs
+++ Multiplayer/GameLobbyServer/Program.cs
@@ -16,40 +16,77 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+List<LobbyServer> lobbies = new List<LobbyServer>();
+
+app.MapPost("/Create a lobby", (string lobbyId, string? hostId) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
+    var newLobby = new LobbyServer
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+        LobbyId = lobbyId,
+        HostId = hostId
+    };
 
+    lobbies.Add(newLobby);
+    return Results.Ok(newLobby);
+});
+
+app.MapPost("/Join lobbies", (string lobbyId, Player player) =>
+{
+    var lobby = lobbies.FirstOrDefault(l => l.LobbyId == lobbyId);
+    if (lobby == null)
+        return Results.NotFound("Lobby not found");
+
+    lobby.Players.Add(player);
+    return Results.Ok("Player added to the lobby");
+});
+
+app.MapGet("/lobbies", () =>
+{
+    return Results.Ok(lobbies);
+});
+
+app.MapPost("/RemovePlayer", (string lobbyId, long playerId) =>
+{
+    var lobby = lobbies.FirstOrDefault(l => l.LobbyId == lobbyId);
+    if (lobby == null)
+        return Results.NotFound("Lobby not found");
+
+    var player = lobby.Players.FirstOrDefault(p => p.Id == playerId);
+    if (player == null)
+        return Results.NotFound("Player not found in the lobby");
+
+    lobby.Players.Remove(player);
+    return Results.Ok($"Player {playerId} removed from lobby {lobbyId}");
+});
+
+app.MapDelete("/CloseLobby", (string lobbyId) =>
+{
+    var lobby = lobbies.FirstOrDefault(l => l.LobbyId == lobbyId);
+    if (lobby == null)
+        return Results.NotFound("Lobby not found");
+
+    lobbies.Remove(lobby);
+    return Results.Ok($"Lobby {lobbyId} closed and removed");
+});
+  
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
 
 // create list of lobbies and store their data
 // create players info
 //
-
 public class Player
 {
     public long Id { get; set; }
     public string? Name { get; set; }
     public bool IsActive { get; set; }
 }
+
+public class LobbyServer
+{
+
+    public string? LobbyId { get; set; }
+    public string? HostId { get; set; }
+    public List<Player> Players { get; set; } = new List<Player>();
+}
+
